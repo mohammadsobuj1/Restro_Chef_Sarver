9bbfb1014e34027dbb922144ede8eb7c80ba3cf4
mohammad sobuj <mohammadsobuj1m>
2023-05-21 17:38:48 +0600

    add redam fime

--- index.js
+++ index.js
@@ -35,9 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
 
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // await client.connect();
+       
         const toyCollactions = client.db("toysDB").collection('toys');
         const heroCOllaction = client.db("toysDB").collection("heros")
 
